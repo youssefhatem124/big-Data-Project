ce0a3b3f4f2b7222c39171d1a7081210cde0752f
youssefhatem124 <51910576+youssefhatem124@users.noreply.github.com>
2021-06-10 13:03:56 +0200

    Add files via upload

---
+++ Load_COVID_TO_HDFS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Landing Zones in Linux and HDFS
+LINUX_LANDING_AREA=/home/cloudera/covid_project/landing_zone/COVID_SRC_LZ
+HDFS_LZ=/user/cloudera/ds/COVID_HDFS_LZ
+
+
+echo "GLOBAL Variables= " $LINUX_LANDING_AREA ", " $HDFS_LZ
+
+
+hdfs dfs -mkdir -p $HDFS_LZ
+echo "COVID_HDFS_LZ CREATED sucessfully"
+
+
+hdfs dfs -put $LINUX_LANDING_AREA/covid-19.csv $HDFS_LZ
+echo "covid-19.csv dataset LOADED sucessfully"
